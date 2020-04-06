3473dac43f49fcc44c9c512809b932773c8df0eb
croi900 <43723625+croi900@users.noreply.github.com>
2020-04-06 23:38:51 +0300

    buton mai bengos

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,26 +1,48 @@
+var f = 1;
+var dragos_gay = 0;
+
 function inapoi()
 {
-
     document.getElementById("muian2").hidden = true;
     $('#muian').animate({
         width: "-=240"
     }, 100);
     scrollBy(0,-100000);
     setTimeout(()=>{document.getElementById("button_back").hidden = false;},1000);
-    f = 1
+    f = 1;
+    dragos_gay = 0;
 }
-var f = 100;
+
+function onload()
+{
+   document.body.addEventListener("click", function(){ 
+        if(dragos_gay)
+        {
+            document.getElementById("muian2").hidden = true;
+            document.getElementById("button_back").hidden = false;
+            $('#muian').animate({
+                width: "-=240"
+            }, 100);
+            f = 1;
+            dragos_gay = 0;
+        }
+    });
+}
 
 function susta()
 {
     var x = document.getElementById("muian")
+
     if(f)
     {
         $('#muian').animate({
             width: "+=240"
         }, 100);
-        setTimeout(()=>{document.getElementById("muian2").hidden = false},101);
+        setTimeout(()=>{document.getElementById("muian2").hidden = false;},101);
         document.getElementById("button_back").hidden = true;
-        f = 0
+        f = 0;
+        setTimeout(() => {
+            dragos_gay = 1;
+        }, 110);
     }
 }
